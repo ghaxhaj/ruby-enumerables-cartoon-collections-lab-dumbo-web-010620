a74e559591c9de7fca004fe44bd686d112c9a1ef
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 04:42:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves# code an argument here
 array = ["Doc", "Dopey", "Bashful", "Grumpy"]
-array.map{ |name| puts name}
+array.map{ |name| puts name.number}
 end
 
 def summon_captain_planet# code an argument here
