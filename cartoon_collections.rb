91d1e91cae1460fad3f723c732a1c41bd4763479
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 04:46:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.each{ |name| puts "#{name}!"
+  planeteer_calls.each{ |name| p "#{name}!"
 end
 
 def long_planeteer_calls# code an argument here
