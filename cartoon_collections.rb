21285e50e9cbad2cceb740d8f9a229f439205a78
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 04:50:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(assorted_words)
+  assorted_words.each{ |word| word.length > 4? return true}
 end
 
 def find_the_cheese# code an argument here
